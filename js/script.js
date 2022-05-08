810023e1a08a61bc3a0a7a6122784ebcfac08563
Adrian Gasiorek <adrian.gasiorek@icloud.com>
2022-05-08 07:52:08 +0200

    Add variables in function call

--- js/script.js
+++ js/script.js
@@ -7,5 +7,5 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = "kamień";
+printMessage(`Zagrałem ${computerMove}! Jeśli Twój ruch to papier, to wygrywasz!`);
